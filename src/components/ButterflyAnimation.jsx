035e2d3786a1880124328640410471b7737911d4
UmarShareef-design <umardesignsgraphic@gmail.com>
2026-02-15 11:07:12 +0530

    Enhance butterfly 3D perspective and flap depth

--- src/components/ButterflyAnimation.jsx
+++ src/components/ButterflyAnimation.jsx
@@ -25,15 +25,16 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                         scale: 1,
                         x: 0,
                         y: 0,
-                        rotate: 0
+                        rotateZ: 0,
+                        rotateX: 45 // Better 3D perspective
                     }}
                     animate={{
                         opacity: [0, 1, 1, 0],
-                        scale: [1, 1.3, 1, 0.6],
+                        scale: [1, 1.4, 1.2, 0.8],
                         // More erratic butterfly-like zig-zag path
-                        x: [0, 30, -40, 60, -20, 120],
-                        y: [0, -40, -100, -180, -280, -450],
-                        rotate: [0, 15, -15, 20, -10, 30],
+                        x: [0, 40, -50, 80, -30, 150],
+                        y: [0, -60, -120, -220, -320, -500],
+                        rotateZ: [0, 20, -20, 30, -15, 40],
                     }}
                     transition={{
                         duration: 4,
@@ -46,25 +47,28 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                         right: '1rem',
                         pointerEvents: 'none',
                         zIndex: 100,
-                        color: '#ff6b6b'
+                        color: '#ffffff',
+                        perspective: '1000px',
+                        transformStyle: 'preserve-3d'
                     }}
                 >
                     {/* The Heart morphing out */}
                     <motion.div
                         initial={{ opacity: 1 }}
                         animate={{ opacity: [1, 0] }}
-                        transition={{ duration: 0.8, delay: 0.5 }}
+                        transition={{ duration: 0.6, delay: 0.5 }}
                         style={{ position: 'absolute', top: 0, left: 0 }}
                     >
-                        <Heart size={24} fill="#ff6b6b" />
+                        <Heart size={24} fill="#ff6b6b" stroke="none" />
                     </motion.div>
 
                     {/* The Butterfly (Double Winged Book) */}
                     <div style={{
                         position: 'relative',
                         display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
                         transformStyle: 'preserve-3d',
-                        perspective: '1000px'
                     }}>
                         {/* Left Wing */}
                         <motion.div
@@ -72,13 +76,13 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                             animate={{
                                 opacity: [0, 1],
                                 scale: [0.5, 1],
-                                rotateY: [0, -80, 0, -80, 0, -80, 0, -80]
+                                rotateY: [0, -110, 0, -110, 0, -110, 0, -110] // Deeper flap
                             }}
                             transition={{
-                                opacity: { duration: 0.8, delay: 0.8 },
-                                scale: { duration: 0.8, delay: 0.8 },
+                                opacity: { duration: 0.6, delay: 0.8 },
+                                scale: { duration: 0.6, delay: 0.8 },
                                 rotateY: {
-                                    duration: 0.5,
+                                    duration: 0.4,
                                     repeat: Infinity,
                                     ease: "easeInOut",
                                     delay: 0.8
@@ -86,12 +90,15 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                             }}
                             style={{
                                 transformOrigin: 'right center',
-                                color: '#ffffff',
-                                width: '12px',
-                                overflow: 'hidden'
+                                width: '14px',
+                                height: '24px',
+                                overflow: 'hidden',
+                                display: 'flex',
+                                justifyContent: 'flex-end',
+                                backfaceVisibility: 'hidden'
                             }}
                         >
-                            <Book size={24} fill="#ffffff" style={{ minWidth: '24px' }} />
+                            <Book size={24} fill="white" stroke="white" />
                         </motion.div>
 
                         {/* Right Wing */}
@@ -100,13 +107,13 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                             animate={{
                                 opacity: [0, 1],
                                 scale: [0.5, 1],
-                                rotateY: [0, 80, 0, 80, 0, 80, 0, 80]
+                                rotateY: [0, 110, 0, 110, 0, 110, 0, 110]
                             }}
                             transition={{
-                                opacity: { duration: 0.8, delay: 0.8 },
-                                scale: { duration: 0.8, delay: 0.8 },
+                                opacity: { duration: 0.6, delay: 0.8 },
+                                scale: { duration: 0.6, delay: 0.8 },
                                 rotateY: {
-                                    duration: 0.5,
+                                    duration: 0.4,
                                     repeat: Infinity,
                                     ease: "easeInOut",
                                     delay: 0.8
@@ -114,12 +121,13 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                             }}
                             style={{
                                 transformOrigin: 'left center',
-                                color: '#ffffff',
-                                width: '12px',
-                                overflow: 'hidden'
+                                width: '14px',
+                                height: '24px',
+                                overflow: 'hidden',
+                                backfaceVisibility: 'hidden'
                             }}
                         >
-                            <Book size={24} fill="#ffffff" style={{ minWidth: '24px', marginLeft: '-12px' }} />
+                            <Book size={24} fill="white" stroke="white" style={{ marginLeft: '-10px' }} />
                         </motion.div>
                     </div>
                 </motion.div>
