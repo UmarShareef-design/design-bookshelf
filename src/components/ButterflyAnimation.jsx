e13dc47f5a4f7df5ac1c594c431c55382cb79292
UmarShareef-design <umardesignsgraphic@gmail.com>
2026-02-15 11:18:21 +0530

    Fix butterfly symmetry and heart morph transition

--- src/components/ButterflyAnimation.jsx
+++ src/components/ButterflyAnimation.jsx
@@ -26,12 +26,11 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                         x: 0,
                         y: 0,
                         rotateZ: 0,
-                        rotateX: 45 // Better 3D perspective
+                        rotateX: 45
                     }}
                     animate={{
                         opacity: [0, 1, 1, 0],
                         scale: [1, 1.4, 1.2, 0.8],
-                        // More erratic butterfly-like zig-zag path
                         x: [0, 40, -50, 80, -30, 150],
                         y: [0, -60, -120, -220, -320, -500],
                         rotateZ: [0, 20, -20, 30, -15, 40],
@@ -47,88 +46,97 @@ const ButterflyAnimation = ({ isTriggered, onComplete }) => {
                         right: '1rem',
                         pointerEvents: 'none',
                         zIndex: 100,
-                        color: '#ffffff',
+                        width: '24px',
+                        height: '24px',
                         perspective: '1000px',
                         transformStyle: 'preserve-3d'
                     }}
                 >
-                    {/* The Heart morphing out */}
-                    <motion.div
-                        initial={{ opacity: 1 }}
-                        animate={{ opacity: [1, 0] }}
-                        transition={{ duration: 0.6, delay: 0.5 }}
-                        style={{ position: 'absolute', top: 0, left: 0 }}
-                    >
-                        <Heart size={24} fill="#ff6b6b" stroke="none" />
-                    </motion.div>
+                    {/* The Morphing Container (Heart to Butterfly) */}
+                    <div style={{ position: 'relative', width: '24px', height: '24px', transformStyle: 'preserve-3d' }}>
 
-                    {/* The Butterfly (Double Winged Book) */}
-                    <div style={{
-                        position: 'relative',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        transformStyle: 'preserve-3d',
-                    }}>
-                        {/* Left Wing */}
+                        {/* The Heart morphing out */}
                         <motion.div
-                            initial={{ opacity: 0, scale: 0.5, rotateY: 0 }}
+                            initial={{ opacity: 1, scale: 1, rotateY: 0 }}
                             animate={{
-                                opacity: [0, 1],
-                                scale: [0.5, 1],
-                                rotateY: [0, -110, 0, -110, 0, -110, 0, -110] // Deeper flap
+                                opacity: [1, 1, 0],
+                                scale: [1, 1.2, 0.8],
+                                rotateY: [0, 90] // Folds away
                             }}
-                            transition={{
-                                opacity: { duration: 0.6, delay: 0.8 },
-                                scale: { duration: 0.6, delay: 0.8 },
-                                rotateY: {
-                                    duration: 0.4,
-                                    repeat: Infinity,
-                                    ease: "easeInOut",
-                                    delay: 0.8
-                                }
-                            }}
-                            style={{
-                                transformOrigin: 'right center',
-                                width: '14px',
-                                height: '24px',
-                                overflow: 'hidden',
-                                display: 'flex',
-                                justifyContent: 'flex-end',
-                                backfaceVisibility: 'hidden'
-                            }}
+                            transition={{ duration: 0.8, delay: 0.5 }}
+                            style={{ position: 'absolute', top: 0, left: 0, width: '24px', height: '24px' }}
                         >
-                            <Book size={24} fill="white" stroke="white" />
+                            <Heart size={24} fill="#ff6b6b" stroke="none" />
                         </motion.div>
 
-                        {/* Right Wing */}
-                        <motion.div
-                            initial={{ opacity: 0, scale: 0.5, rotateY: 0 }}
-                            animate={{
-                                opacity: [0, 1],
-                                scale: [0.5, 1],
-                                rotateY: [0, 110, 0, 110, 0, 110, 0, 110]
-                            }}
-                            transition={{
-                                opacity: { duration: 0.6, delay: 0.8 },
-                                scale: { duration: 0.6, delay: 0.8 },
-                                rotateY: {
-                                    duration: 0.4,
-                                    repeat: Infinity,
-                                    ease: "easeInOut",
-                                    delay: 0.8
-                                }
-                            }}
-                            style={{
-                                transformOrigin: 'left center',
-                                width: '14px',
-                                height: '24px',
-                                overflow: 'hidden',
-                                backfaceVisibility: 'hidden'
-                            }}
-                        >
-                            <Book size={24} fill="white" stroke="white" style={{ marginLeft: '-10px' }} />
-                        </motion.div>
+                        {/* The Butterfly (Double Winged Book) */}
+                        <div style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            transformStyle: 'preserve-3d',
+                            width: '24px',
+                            height: '24px'
+                        }}>
+                            {/* Left Wing */}
+                            <motion.div
+                                initial={{ opacity: 0, rotateY: 90 }}
+                                animate={{
+                                    opacity: [0, 1],
+                                    rotateY: [90, -110, 0, -110, 0, -110, 0, -110]
+                                }}
+                                transition={{
+                                    opacity: { duration: 0.4, delay: 0.8 },
+                                    rotateY: {
+                                        duration: 0.4,
+                                        repeat: Infinity,
+                                        ease: "easeInOut",
+                                        delay: 0.8
+                                    }
+                                }}
+                                style={{
+                                    transformOrigin: 'right center',
+                                    width: '12px',
+                                    height: '24px',
+                                    overflow: 'hidden',
+                                    display: 'flex',
+                                    justifyContent: 'flex-end',
+                                    backfaceVisibility: 'hidden'
+                                }}
+                            >
+                                <Book size={24} fill="white" stroke="white" />
+                            </motion.div>
+
+                            {/* Right Wing */}
+                            <motion.div
+                                initial={{ opacity: 0, rotateY: -90 }}
+                                animate={{
+                                    opacity: [0, 1],
+                                    rotateY: [-90, 110, 0, 110, 0, 110, 0, 110]
+                                }}
+                                transition={{
+                                    opacity: { duration: 0.4, delay: 0.8 },
+                                    rotateY: {
+                                        duration: 0.4,
+                                        repeat: Infinity,
+                                        ease: "easeInOut",
+                                        delay: 0.8
+                                    }
+                                }}
+                                style={{
+                                    transformOrigin: 'left center',
+                                    width: '12px',
+                                    height: '24px',
+                                    overflow: 'hidden',
+                                    backfaceVisibility: 'hidden'
+                                }}
+                            >
+                                <Book size={24} fill="white" stroke="white" style={{ marginLeft: '-12px' }} />
+                            </motion.div>
+                        </div>
                     </div>
                 </motion.div>
             )}
