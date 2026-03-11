6ed8f08e4b5d0c0e976e476e1dfea349ccd3f435
UmarShareef-design <umardesignsgraphic@gmail.com>
2026-03-11 20:16:49 +0530

    fix: update sitemap configuration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://uxbooks.in',
     integrations: [react(), sitemap()],
+    redirects: {
+        '/sitemap.xml': '/sitemap-index.xml'
+    }
 });
